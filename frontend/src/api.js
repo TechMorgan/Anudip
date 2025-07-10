5c664f5497d91f62d92997cf3e19543f186d07a9
TechMorgan <83485038+TechMorgan@users.noreply.github.com>
2025-07-10 20:24:04 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://anudip-production.up.railway.app:8080/api',
+  baseURL: 'anudip-production.up.railway.app:8080/api',
 });
 
 api.interceptors.request.use((config) => {
