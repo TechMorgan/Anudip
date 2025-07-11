abeffb22c7a9ded76e4bae5ac07cb68bfcdd2ebc
TechMorgan <83485038+TechMorgan@users.noreply.github.com>
2025-07-11 08:08:57 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://mysql-production-ee23.up.railway.app:33060/api',
+  baseURL: 'https://mysql-production-ee23.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
